4b9ce26321670420aff4f11d360a4a786d47ab44
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-18 14:07:32 +0300

    Add files via upload

---
+++ ClassBoxData/Box.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box()
+        {
+
+        }
+        public Box(double length, double width, double height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+            Length = length;
+            Width = width;
+            Height = height;
+        
+        }
+
+        public double Length
+        {
+            get 
+            {
+                return this.length;
+            }
+            private set 
+            {
+                if(this.length <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+            } 
+        }
+        public double Width
+        {
+            get 
+            {
+                return this.width;
+            }
+            private set
+            {
+                if(this.width <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+            }
+        }
+        public double Height
+        {
+            get 
+            { 
+                return this.height; 
+            }
+            private set 
+            {
+                if(this.height <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+            }
+        }
+        public double SurfaceArea()
+        {
+            double surfaceArea = 2 * (this.length * this.width) + 2 * (this.length * height) + 2 * (width * height);
+            return surfaceArea;
+        }
+        public double LateralSurfaceArea()
+        {
+            double laternalSurface = 2 * (this.length * this.height) + 2 * (width * height);
+            return laternalSurface;
+        }
+        public double Volume()
+        {
+            double volume = length * width * height;
+            return volume;
+        }
+
+    }
+}
---
+++ ClassBoxData/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double input = 0;
+            double lenght = 0;
+            double width = 0;
+            double height = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                input = double.Parse(Console.ReadLine());
+                if(i == 0)
+                {
+                    lenght = input;
+                }
+                else if(i == 1)
+                {
+                    width = input;
+                }
+                else if(i == 2)
+                {
+                    height = input;
+                }
+
+            }
+            Box box = new Box();
+            try
+            {
+                box = new Box(lenght, width, height);
+            }
+            catch(ArgumentException argument)
+            {
+                Console.WriteLine(argument.Message);
+                return;
+            }
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
+        }
+    }
+}
