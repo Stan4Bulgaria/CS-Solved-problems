509ba509db121bd04caea1f2cc1de5b8af8276dc
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 10:32:39 +0300

    Add files via upload

---
+++ Motorcycle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Motorcycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
