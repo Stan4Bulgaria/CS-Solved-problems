489c1acf72a7679942325fc6503b14a83dc34873
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 10:33:50 +0300

    Add files via upload

---
+++ Person/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private int age;
+        private string name;
+
+        public Person( string name, int age)
+        {
+            Age = age;
+            Name = name;
+            this.age = age;
+            this.name = name;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(String.Format($"Name: {Name}, Age: {Age}"));
+            return sb.ToString();
+
+        }
+    }
+}
