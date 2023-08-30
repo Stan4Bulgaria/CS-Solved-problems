f3a18076d92798cfcb67504626fb3d49fcfa82b8
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 10:39:03 +0300

    Add files via upload

---
+++ MilitaryElite/LieutenantGeneral.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite
+{
+    public class LieutenantGeneral:Private
+    {
+        public LieutenantGeneral(string id,string firstName ,string lastName, decimal salary):base(id,firstName,lastName,salary)
+        {
+            Privates = new Dictionary<string, Private>();
+        }
+        public Dictionary<string,Private> Privates { get; set; }
+    }
+}
---
+++ MilitaryElite/Private.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite
+{
+    public class Private:Soldier
+    {
+        public Private(string id,string firstName,string lastName,decimal salary):base(id,firstName,lastName)
+        {
+            this.Salary = salary;
+        }
+        public decimal Salary { get; set; }
+    }
+}
