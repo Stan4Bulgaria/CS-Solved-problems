9b504a901f95a2d685ac1be932979fc0e86c9adf
Stanislav <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-08-30 10:34:49 +0300

    Add files via upload

---
+++ PlayersAndMonsters/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private int level;
+        private string username;
+
+        public Hero(string username, int level)
+        {
+            Username = username;
+            Level = level;
+            this.level = level;
+            this.username = username;   
+        }
+
+        public string Username { get; set; }
+        public int Level { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(String.Format($"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}"));
+            return sb.ToString();
+        }
+    }
+}
---
+++ PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Wizard wizard = new Wizard("xxxYourMomsLoverxxx", 120);
+            Console.WriteLine(wizard.ToString());
+        }
+    }
+}
